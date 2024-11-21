b4dab6d1e9e2ec78cd2997302bbcc80882d923c3
shantpr05 <shanthi.parvatham@hyperisland.se>
2024-11-22 00:46:28 +0100

    adding modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,33 @@
-// webpack.config.js
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  mode: 'development', // Use 'production' for production builds
+  entry: './src/index.js', // Main entry file
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
+    filename: 'main.js', // Output bundle
+    path: path.resolve(__dirname, 'dist'), // Output directory
+    clean: true, // Cleans the output directory before each build
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
+  devServer: {
+    static: './dist', // Serve files from the dist folder
+    open: true, // Automatically open the browser
+  },
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        test: /\.css$/, // Handles CSS files
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i, // Handles image files
+        type: 'asset/resource',
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html', // Use your custom HTML as a template
+    }),
+  ],
 };
